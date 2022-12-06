76b3b6d2986a7c7b085e5b6adc5004f003f95366
novanrohman <novanrohman@gmail.com>
2022-12-06 12:21:43 +0700

    add mobil

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('type')->default(0); /* Users: 0=>User, 1=>Owner, 2=>admin */
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
