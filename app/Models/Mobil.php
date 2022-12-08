dc5d0f5857e639caf43d96442d29d36b20d68ff9
novanrohman <novanrohman@gmail.com>
2022-12-08 21:18:16 +0700

    fixed CR tb_mobil

---
+++ app/Models/Mobil.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mobil extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_detail_owner','nama_mobil', 'jenis_mobil', 'harga', 'deskripsi', 'jumlah_kursi', 'bahan_bakar', 'jenis_transmisi' 
+    ];
+}
