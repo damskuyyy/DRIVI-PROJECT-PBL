3ceaf7603df528cea941acada41b80fff2804f4a
lutfitasari <lutfitayunita@gmail.com>
2022-12-09 12:43:12 +0700

    fita

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('type')->default(0); /* Users: 0=>User, 1=>Owner, 2=>admin */
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
