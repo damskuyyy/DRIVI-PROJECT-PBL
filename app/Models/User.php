dbc7678595aebe05b7fcba01fc76a0e08bd91e4e
novanrohman <novanrohman@gmail.com>
2022-12-10 20:26:13 +0700

    multilavel user fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Passport\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -26,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
