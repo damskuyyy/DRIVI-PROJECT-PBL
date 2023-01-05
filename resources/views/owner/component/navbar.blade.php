7138fa527eb8f42ab155cb49b361175cc3b99c91
faqihul <muqoddam2711@gmail.com>
2023-01-05 07:16:56 +0700

    second commit

--- resources/views/owner/component/navbar.blade.php
+++ resources/views/owner/component/navbar.blade.php
@@ -177,7 +177,7 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="{{('admin/profile')}}">
+                <a class="dropdown-item" href="{{url('admin.profile')}}">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
--- resources/views/user/homepage.blade.php
+++ resources/views/user/homepage.blade.php
@@ -34,7 +34,7 @@
             <button type="button" class="btn btn-outline-dark">Register</button>
             </li> --}}
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+              <a class="nav-link dropdown-toggle" href="{{ url('user.profile') }}" id="userDropdown" role="button"
                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <span class="mr-2 d-none d-lg-inline text-gray-600 small">User</span>
                   <img class="img-profile rounded-circle"
